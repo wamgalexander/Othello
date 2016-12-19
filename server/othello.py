7ecffb41ddc78c139495040451bdb783a1e0422c
wamgalexander <wamgalexander@gmail.com>
2016-12-20 00:29:20 +0800

    merge MultCtrlBlock & master

--- server/othello.py
+++ server/othello.py
@@ -2,7 +2,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QDesktopWidget
 from PyQt5.QtCore import pyqtSignal, QTimer
 import functools
-
+import sys
 import constants
 
 class QRightClickButton(QtWidgets.QPushButton):
@@ -629,3 +629,23 @@ class Ui_MainWindow(object):
 		# start QTimer (start to blink)
 		for i in range(0, 4):
 			self.timer[i].start(constants.ONE_SEC/constants.FREQ[i])
+
+class ExampleApp(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        # Explaining super is out of the scope of this article
+        # So please google it if you're not familar with it
+        # Simple reason why we use it here is that it allows us to
+        # access variables, methods etc in the design.py file
+        super(self.__class__, self).__init__()
+        self.setupUi(self)  # This is defined in design.py file automatically
+                            # It sets up layout and widgets that are defined
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)  # A new instance of QApplication
+    form = ExampleApp()                 # We set the form to be our ExampleApp (design)
+    form.show()                         # Show the form
+    app.exec_()                         # and execute the app
+
+
+if __name__ == '__main__':              # if we're running file directly and not importing it
+    main()                              # run the main function
